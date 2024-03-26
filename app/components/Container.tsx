8ab63a1154e64038b9d8b4e6a7e439f2e5bd19e3
AleWaweru <ngashalex8@gmail.com>
2024-03-26 14:28:09 +0300

    add images

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from "react";
-
 interface ContainerProps {
     children: React.ReactNode;
   }
   
   const Container: React.FC<ContainerProps> = ({ children }) => {
     return (
-      <Suspense>
         <div className="max-w-[1920px] mx-auto xl:px-20 md:px-2 px-4">{children}</div>
-      </Suspense>
     );
   }
   
